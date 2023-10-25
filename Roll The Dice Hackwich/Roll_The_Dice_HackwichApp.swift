b67a7eeaab6f8f74f2dd9b2b562f89a85901ecd9
Phillip Mantatsky <pmantatsky1@bsd220.org>
2023-10-25 09:46:43 -0500

    Initial Commit

---
+++ Roll The Dice Hackwich/Roll_The_Dice_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_The_Dice_HackwichApp.swift
+//  Roll The Dice Hackwich
+//
+//  Created by Phillip Mantatsky on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Roll_The_Dice_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
